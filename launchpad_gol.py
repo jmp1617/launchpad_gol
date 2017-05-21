b65d161a27ccf0a3a8835601b4dda66c6c446ae4
Jacob Potter <jmp1617@rit.edu>
2017-05-21 11:13:06 -0400

    added mean python file

---
+++ launchpad_gol.py
@@ -0,0 +1,14 @@
+#Game of Life on the Novation Launchpad 
+
+import sys
+
+try:
+	import launchpad_py as launchpad
+except ImportError:
+	try:
+		import launchpad
+	except ImportError:
+		sys.exit("error loading launchpad.py")
+
+import random
+from pygame import time
