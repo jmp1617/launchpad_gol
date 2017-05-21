6022d9290ae49201534fa5267e9de8a0f07b0cee
Jacob Potter <jmp1617@rit.edu>
2017-05-21 18:17:34 -0400

    corrected imports

--- launchpad_gol.py
+++ launchpad_gol.py
@@ -1,14 +1,6 @@
-#Game of Life on the Novation Launchpad 
+# Game of Life on the Novation Launchpad
 
-import sys
-
-try:
-	import launchpad_py as launchpad
-except ImportError:
-	try:
-		import launchpad
-	except ImportError:
-		sys.exit("error loading launchpad.py")
-
+import launchpad_py as launchpad
 import random
 from pygame import time
+
